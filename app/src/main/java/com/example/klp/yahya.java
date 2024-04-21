7490e20cd7b111e3902df74510304942d1527487
Nur Yahya Akhmad <yahyanur246@gmail.com>
2024-04-21 17:06:12 +0800

    tes

---
+++ app/src/main/java/com/example/klp/yahya.java
@@ -0,0 +1,14 @@
+package com.example.klp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class yahya extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tabungan);
+    }
+}
